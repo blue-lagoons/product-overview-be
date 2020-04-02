e8d060ea96524e6656039a6351c8adceca414b62
Michael Morahan <morahan.mike@gmail.com>
2020-04-01 21:21:43 -0600

    Added format of data from api as an OBJ

---
+++ noSQL/schema.js
@@ -0,0 +1,22 @@
+{
+    "id": 2,
+    "name": "Bright Future Sunglasses",
+    "slogan": "You've got to wear shades",
+    "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+    "category": "Accessories",
+    "default_price": "69",
+    "features": [
+        {
+            "feature": "Lenses",
+            "value": "Ultrasheen"
+        },
+        {
+            "feature": "UV Protection",
+            "value": "null"
+        },
+        {
+            "feature": "Frames",
+            "value": "LightCompose"
+        }
+    ]
+}
