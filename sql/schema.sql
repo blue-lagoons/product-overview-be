86619ef26de5b4a2a76ec5984dd7fd3edb80e7a6
Michael Morahan <morahan.mike@gmail.com>
2020-04-02 19:06:58 -0600

    Updated Databases for importing to 5 tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,19 +3,43 @@ DROP DATABASE IF EXISTS ;
 CREATE DATABASE product-overview;
 
 CREATE TABLE products (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     product_id INTEGER,
     name VARCHAR(100),
     slogan VARCHAR(250),
     description VARCHAR(500),
     category VARCHAR(100),
-    default_price INT,
-    -- features Foreign Key 
+    default_price INTEGER,
+    features INTEGER Foreign Key 
 )
 
 CREATE TABLE features (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER FOREIGN KEY,
     feature VARCHAR(100),
     value VARCHAR(100)
+)
+
+CREATE TABLE styles (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER FOREIGN KEY,
+    name VARCHAR(100),
+    sale_price INTEGER,
+    original_price INTEGER,
+    default_style VARCHAR(100)
+)
+
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    style_id INTEGER FOREIGN KEY,
+    url VARCHAR(150)
+)
+
+CREATE TABLE skus (
+    id SERIAL PRIMARY KEY,
+    style_id INTEGER FOREIGN KEY,
+    size VARCHAR(25),
+    quantity INTEGER
 )
 
 
