9993abd01e1609a927d28d0b1034ddcc0da24e98
Michael Morahan <morahan.mike@gmail.com>
2020-04-06 12:30:31 -0600

    set up faker import

---
+++ sql/product-gen.js
@@ -0,0 +1,27 @@
+const faker = require('faker');
+const fs = require('fs');
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+const t0 = performance.now();
+let id = 1000012;
+let finalProductId = 10000000;
+
+//id,name,slogan,description,category,default_price
+while (id <= finalProductId) {
+    let name = faker.hacker.adjective() + " " + faker.hacker.noun();
+    let slogan = faker.company.catchPhrase();
+    let description = faker.lorem.sentence();
+    let category = faker.random.arrayElement(["Kicks", "Accessories", "Skirt", "Suit", "Slacks", "Shorts", "Cap", "Jacket", "Pants", "Heels", "Socks", "Backpack", "Sunglasses","Dress","Shoes","Coat","Romper"]);
+    let default_price = faker.random.number({ min: 1, max: 1023 });
+
+    fs.appendFileSync('csvFiles/generated-products.csv',
+        `${id},"${name}","${slogan}","${description}","${category}",${default_price},\n`
+    );
+    ++id;
+};
+
+const t1 = preformance.now();
+console.log('execution time (ms): ', t1 - t0);
+console.log('execution time (min): ', (t1 - t0) / (60000));
+
+// psql - U postgres - f '/Users/mmorahan/Desktop/SDC/Repos/product-overview-be/sql/product-gen.js'
