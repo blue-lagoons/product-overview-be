d32f9b31cf2b811adf9d45312d0065495c937267
Michael Morahan <morahan.mike@gmail.com>
2020-04-12 21:31:45 -0600

    edited random file to utilize creating random number
    avoided math.random for true randomness.

--- sql/db/randomizer.js
+++ sql/db/randomizer.js
@@ -8,6 +8,13 @@ function generateRandomData(userContext, events, done) {
   return number();
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(10000000);
+  return Math.floor(Math.random() * (10000000 - 1)) + 1; //The maximum is exclusive and the minimum is inclusive
+}
+
+
 module.exports = {
-  generateRandomData
+  getRandomInt
 };
