9798cfb162a6b6028bcef7fea909961b53995546
Michael Morahan <morahan.mike@gmail.com>
2020-04-12 22:05:33 -0600

    reverting back to orginal random num

--- sql/db/randomizer.js
+++ sql/db/randomizer.js
@@ -1,19 +1,19 @@
-// const Faker = require("faker");
+const Faker = require("faker");
 
-// function generateRandomData(userContext, events, done) {
-//   // generate data with Faker:
-//   const number = faker.random.number({ min: 9000000, max: 10000000 });
-
-//   // continue with executing the scenario:
-//   return number();
-// }
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+function generateRandomData(userContext, events, done) {
+  // generate data with Faker:
+  const number = faker.random.number({ min: 9000000, max: 10000000 });
+  
+  // continue with executing the scenario:
+  return number();
 }
-getRandomInt(1, 10000000);
+
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+// }
+// getRandomInt(1, 10000000);
 
 module.exports = {
   getRandomInt
