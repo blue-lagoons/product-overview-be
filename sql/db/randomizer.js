00b59db5ac9d6e683d05703ea8040d6e8ca24bf5
Michael Morahan <morahan.mike@gmail.com>
2020-04-12 21:57:51 -0600

    fixed randomizer bug

--- sql/db/randomizer.js
+++ sql/db/randomizer.js
@@ -1,20 +1,21 @@
-const Faker = require("faker");
+// const Faker = require("faker");
 
-function generateRandomData(userContext, events, done) {
-  // generate data with Faker:
-  const number = faker.random.number({ min: 9000000, max: 10000000 });
+// function generateRandomData(userContext, events, done) {
+//   // generate data with Faker:
+//   const number = faker.random.number({ min: 9000000, max: 10000000 });
 
-  // continue with executing the scenario:
-  return number();
-}
+//   // continue with executing the scenario:
+//   return number();
+// }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(10000000);
-  return Math.floor(Math.random() * (10000000 - 1)) + 1; //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
-
+getRandomInt(1, 10000000);
 
 module.exports = {
   getRandomInt
+  // generateRandomData
 };
