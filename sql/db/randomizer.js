7416fe929f0720e11214c6d92b3a9b54140ba3a1
Michael Morahan <morahan.mike@gmail.com>
2020-04-13 19:29:59 -0600

    debug

--- sql/db/randomizer.js
+++ sql/db/randomizer.js
@@ -1,21 +1,28 @@
-const Faker = require("faker");
+'use strict';
+
+const faker = require("faker");
 
 function generateRandomData(userContext, events, done) {
   // generate data with Faker:
-  const number = faker.random.number({ min: 9000000, max: 10000000 });
-  
-  // continue with executing the scenario:
-  return number();
+  const number = faker.random.number({ min: 9000000, max: 10000000 }); 
+  const url = `/products/${number}`;
+
+  return url;
 }
 
-// function getRandomInt(min, max) {
+
+// function getRandomInt(min = 1, max = 10000000) {
 //   min = Math.ceil(min);
 //   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+//   let x = Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+//   return x
 // }
-// getRandomInt(1, 10000000);
+// // console.log(getRandomInt())
+// // let x = 1;
+
+console.log("calling script ", generateRandomData())
 
 module.exports = {
-  // getRandomInt
+  // getRandomInt,
   generateRandomData
 };
