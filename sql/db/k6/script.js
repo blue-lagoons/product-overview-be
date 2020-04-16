0adddbe9598594f56533eae4aefdf167a63785cb
Michael Morahan <morahan.mike@gmail.com>
2020-04-15 20:29:16 -0600

    created two versions (1 w/ many page visits)

--- sql/db/k6/script.js
+++ sql/db/k6/script.js
@@ -5,31 +5,38 @@ import { Rate } from "k6/metrics";
 export let errorRate = new Rate("errors");
 
 
+// user visits 150 product pages
+// export default function () {
+//   var url = `http://localhost:3000/products/${Math.ceil(Math.random() * 1000000)}`;
+//   for (let i = 0; i < 150; i++){
+//       check(http.get(url), {
+//         "status is 200": (r) => r.status == 200,
+//       }) || errorRate.add(1);
+//       sleep(0.1);
+//   }
+// }
 
+// user visits 1 product page
 export default function () {
   var url = `http://localhost:3000/products/${Math.ceil(Math.random() * 1000000)}`;
-  for (let i = 0; i < 150; i++){
-      check(http.get(url), {
-        "status is 200": (r) => r.status == 200,
-      }) || errorRate.add(1);
-      sleep(0.01);
-    }
-  }
+  check(http.get(url), {
+    "status is 200": (r) => r.status == 200,
+  }) || errorRate.add(1);
+}
 
 export let options = {
     stages: [
-        { duration: '5s', target: 10},
-        { duration: '15s', target: 1000},
-        { duration: '5s', targer: 0}
+        // { duration: '5s', target: 10},
+        { duration: '25s', target: 2500},
+        // { duration: '5s', targer: 0}
     ],
     throw: true,         // throw errors on failed HTTP requests
     // discardResponseBodies: true, //discard repsponse bodies to lessen the amount of mem required
-    thresholds: {http_req_duration: ["max<2000"]},
+    thresholds: {
+        http_req_duration: ["max<2000"],
+        errors: ["rate<0.1"], // test passes if error rate is less than 10 %
+    },
 };
-
-
-
-
 
 // ======= g
 
